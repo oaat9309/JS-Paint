c86dd5d2f75004a872306ead2f274816bff7d7d8
kite1993 <kite1993@naver.com>
2021-10-10 15:53:14 +0900

    10.10
    - 색깔 지정 기능 추가
    - 커서를 이용한 선 긋기 기능 추가
    - 채우기 기능 추가
    - 저장 기능 추가

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,27 @@
 const canvas = document.querySelector(".jsCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
-canvas.height = 300;
+const colors = document.getElementsByClassName("jsColor");
+const range = document.querySelector(".jsRange");
 
-ctx.strokeStyle = "#2c2c2c";
+const mode = document.querySelector(".jsMode");
+
+const saveBtn = document.querySelector(".jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+
+canvas.width = document.getElementsByClassName("jsCanvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("jsCanvas")[0].offsetHeight;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+
+let filling = false;
 
 function onMouseMove(event) {
     const x = event.offsetX;
@@ -22,10 +36,6 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
-
 function stopPainting(event) {
     painting = false;
 }
@@ -34,12 +44,67 @@ function startPainting() {
     painting = true;
 }
 
+function changeColor(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleJsMode(event) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill"
+    }
+    else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick(event) {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+//우클릭 (contextmenu 방지하는 함수)
+function handleCm(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs";
+    link.click();
+}
 
 if(canvas) {
         canvas.addEventListener("mousemove", onMouseMove);
         canvas.addEventListener("mousedown", startPainting);
         canvas.addEventListener("mouseup", stopPainting);
         canvas.addEventListener("mouseleave", stopPainting);
+        canvas.addEventListener("click", handleCanvasClick);
+        canvas.addEventListener("contextmenu", handleCm)
 }
 
+Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleJsMode);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick)
+}
 
